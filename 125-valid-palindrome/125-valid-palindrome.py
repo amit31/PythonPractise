10d9844b18b5b930d1afbb46499569535930616a
Amit Sharma <lucifier.amit@gmail.com>
2022-02-06 17:11:19 +0530

    Time: 70 ms (40.20%), Space: 16.5 MB (14.03%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -5,15 +5,10 @@ class Solution:
          strp=strp.lower()
          strp=[strp for strp in strp]
          print(strp)
-         left=0
-         count=0
+         
          right=len(strp)-1
          for i in range(0,right):
             if strp[i]!=strp[right]:
-                print(strp[left])
-                print(strp[right])
-                count=count+1
-                print(count)
                 return False
             else:
                 right=right-1
