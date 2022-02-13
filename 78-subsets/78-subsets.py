e1102573379bb8345d9e713670f8ccea1304348f
Amit Sharma <lucifier.amit@gmail.com>
2022-02-13 20:58:22 +0530

    Time: 46 ms (49.95%), Space: 14.1 MB (82.34%) - LeetHub

---
+++ 78-subsets/78-subsets.py
@@ -0,0 +1,21 @@
+class Solution:
+    def solution(self,nums,ans,cur,index):
+        
+        if(index>len(nums)):
+             return
+        ans.append(cur[:])
+        for i in range(index,len(nums)):
+            if nums[i] not in cur:
+                cur.append(nums[i])
+                self.solution(nums,ans,cur,i+1)
+                cur.pop()
+        return 
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+         ans = []
+         cur = []
+         self.solution(nums,ans,cur,0)
+         return ans
+            
+            
+            
+        
