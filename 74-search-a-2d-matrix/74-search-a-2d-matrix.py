61c6da222204fcf65f5fea1261dbace73ddd3dae
Amit Sharma <lucifier.amit@gmail.com>
2022-03-30 08:34:49 +0530

    Time: 52 ms (73.01%), Space: 14.7 MB (8.89%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -0,0 +1,29 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        dictd={}
+        size=len(matrix)
+        print(size)
+        k=0
+        if size==1:
+            print("i am here")
+            for i in range(0,len(matrix)):
+                for j in range(0,len(matrix[0])):
+                    k=matrix[i][j]
+                    dictd[k]=dictd.get(k,0)+1
+            print(dictd)
+        
+            for key,value in dictd.items():
+             if key == target:
+                return True
+            return False
+            
+        for i in range(0,len(matrix)):
+            for j in range(0,len(matrix[0])):
+                 k=matrix[i][j]
+                 dictd[k]=dictd.get(k,0)+1
+        print(dictd)
+        
+        for key,value in dictd.items():
+            if key == target:
+                return True
+        return False
