53f53f979d6775ffdf854e353c1f46a2e0ef734e
Amit Sharma <lucifier.amit@gmail.com>
2022-03-30 14:27:05 +0530

    Time: 48 ms (51.86%), Space: 14.2 MB (35.70%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,15 +1,50 @@
+
+"""
+def makesubsets(nums[],cur[],ans[],i)
+
+base case if index > len(nums):
+             return
+      ans.append(cur[:])
+      algo: 
+      loop from i to len(nums)
+        if nums[i] not in cur
+         cur.append(nums[i])-->take case
+         makesubsets(nums,cur,ans,i)
+         cur.pop()-->remove case
+
+Always the pattern is :
+
+123
+1 3
+12
+ 23
+  3
+ 2
+1
+none selected
+
+ number of subsets is 2^n as choice is 2 for all inputs(n)
+
+"""
+
+
 class Solution:
     
     def makesubsets(self,nums:List[int],ans:List[int],cur:List[int],index):
         if index >= len(nums):
             return
-        ans.append(cur[:])
-        for i in range(index,len(nums)):
-            if nums[i] not in cur:
-                cur.append(nums[i])
-                self.makesubsets(nums,ans,cur,i)
-                cur.pop()
-        
+        if cur not in ans:
+            ans.append(cur[:])
+#         for i in range(index,len(nums)):
+#             if nums[i] not in cur:
+#                 cur.append(nums[i])
+#                 self.makesubsets(nums,ans,cur,i)
+#                 cur.pop()
+        if nums[index] not in cur:
+            cur.append(nums[index])
+            self.makesubsets(nums,ans,cur,index)
+            cur.pop()
+        self.makesubsets(nums,ans,cur,index+1)  
         return
     
     def subsets(self, nums: List[int]) -> List[List[int]]:
