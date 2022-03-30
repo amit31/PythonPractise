0d0dcb30135bb1c31482f7f506464dfc73d74d9a
Amit Sharma <lucifier.amit@gmail.com>
2022-03-30 08:36:23 +0530

    Time: 127 ms (5.23%), Space: 14.8 MB (8.89%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -4,19 +4,7 @@ class Solution:
         size=len(matrix)
         print(size)
         k=0
-#         if size==1:
-#             print("i am here")
-#             for i in range(0,len(matrix)):
-#                 for j in range(0,len(matrix[0])):
-#                     k=matrix[i][j]
-#                     dictd[k]=dictd.get(k,0)+1
-#             print(dictd)
-        
-#             for key,value in dictd.items():
-#              if key == target:
-#                 return True
-#             return False
-            
+
         for i in range(0,len(matrix)):
             for j in range(0,len(matrix[0])):
                  k=matrix[i][j]
