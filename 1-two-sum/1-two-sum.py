735237c09f07d45324ec1f067216c5b9a50ca4d7
Amit Sharma <lucifier.amit@gmail.com>
2022-04-02 06:51:55 +0530

    Time: 64 ms (89.95%), Space: 15.2 MB (51.58%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        m={}
+        n=len(nums)
+        # for i in range(0,n):
+        #     goal=target-nums[i]
+        #     if goal in m:
+        #         return m[goal],i
+        #     else:
+        #         m[nums[i]]=i
+            
+        for i in range(0,n):
+            goal = target - nums[i]
+            if goal in m:
+                return m[goal],i
+            else:
+                m[nums[i]]=i
