7451e18df979719e408bea1d916497d4f3a1a262
Amit Sharma <lucifier.amit@gmail.com>
2022-04-09 18:03:23 +0530

    Time: 1321 ms (10.74%), Space: 0B (100.00%) - LeetHub

---
+++ 185-department-top-three-salaries/185-department-top-three-salaries.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+
+
+select temp.name as Department,temp.employeename as Employee ,temp.salary as Salary   from
+(select d.name,e.name as employeename,salary,dense_rank()   over ( partition by d.name order by salary desc ) as dr from employee e join department d on e.departmentId=d.id) temp where temp.dr in (1,2,3)
