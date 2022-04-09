3e0249e975cc3a3d00c3cac75e46f4be70cd1a22
Amit Sharma <lucifier.amit@gmail.com>
2022-04-09 18:18:25 +0530

    Update 185-department-top-three-salaries.sql

--- 185-department-top-three-salaries/185-department-top-three-salaries.sql
+++ 185-department-top-three-salaries/185-department-top-three-salaries.sql
@@ -2,4 +2,5 @@
 
 
 select temp.name as Department,temp.employeename as Employee ,temp.salary as Salary   from
-(select d.name,e.name as employeename,salary,dense_rank()   over ( partition by d.name order by salary desc ) as dr from employee e join department d on e.departmentId=d.id) temp where temp.dr in (1,2,3)
+(select d.name,e.name as employeename,salary,dense_rank()   over ( partition by d.name order by salary desc ) as dr from employee e join department d on e.departmentId=d.id)
+temp where temp.dr in (1,2,3)
