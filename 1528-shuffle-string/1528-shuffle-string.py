1fe28e15534f6e1116b2df2a60a6fb90ef127395
Amit Sharma <lucifier.amit@gmail.com>
2022-04-15 21:10:21 +0530

    Time: 71 ms (62.97%), Space: 14 MB (18.19%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.py
@@ -0,0 +1,28 @@
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        j=[]
+        m={}
+        k={}
+        ans=""
+            
+        k=0
+        for i in indices:
+            m[i]=s[k]
+            
+            k=k+1
+        
+        print (m)
+        
+        
+        k=sorted(m.items(),key=lambda x:x[0])
+        
+        print(k)
+        
+        for i in k:
+            j.append(i[1])
+        
+        return ans.join(j)
+        
+            
+            
+            
