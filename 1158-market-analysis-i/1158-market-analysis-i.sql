0864661412f5b31634c42ae66623f0c1800ed9c4
Amit Sharma <lucifier.amit@gmail.com>
2022-04-17 17:58:53 +0530

    Time: 935 ms (92.24%), Space: 0B (100.00%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -1,6 +1,9 @@
 # Write your MySQL query statement below
 
 
+#where filters the rows when used with order_date
+#and will give result with left join even when condition fails
+
 select user_id as buyer_id,join_date,count(o.order_date) as orders_in_2019 from  users u 
 left join orders o on u.user_id=o.buyer_id
 and o.order_date 
