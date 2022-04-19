df5a2b36b03034c469c41eef74453a70f4e6b86a
Amit Sharma <lucifier.amit@gmail.com>
2022-04-19 12:24:22 +0530

    Time: 567 ms (30.73%), Space: 0B (100.00%) - LeetHub

--- 180-consecutive-numbers/180-consecutive-numbers.sql
+++ 180-consecutive-numbers/180-consecutive-numbers.sql
@@ -1,8 +1,30 @@
 # Write your MySQL query statement below
 
 
-select  distinct l1.num as ConsecutiveNums  from logs l1 
-join logs l2 on l1.id=l2.id+1
-and l1.num=l2.num
-join logs l3 on l1.id=l3.id+2
-and l1.num=l3.num
+# select num as  ConsecutiveNums from
+
+# () 
+
+# where count()
+
+# SELECT DISTINCT num
+# FROM
+# (
+# SELECT num,LEAD(num) OVER(ORDER BY id) AS lead, LAG(num) OVER (ORDER BY id) AS lag
+# FROM logs
+# )t
+# WHERE num=lead and num=lag;
+
+select distinct num as ConsecutiveNums  from
+(SELECT num,LEAD(num,1) OVER(ORDER BY id) AS lead1, 
+LEAD(num,2) OVER(ORDER BY id) AS lead2, 
+LAG(num,1) OVER (ORDER BY id) AS lag1,
+LAG(num,2) OVER (ORDER BY id) AS lag2 from logs)t1
+where num =t1.lead1 and num=t1.lead2
+OR num=t1.lag1 and num = t1.lag2
+OR num=t1.lag1 and num=t1.lead1
+
+# SELECT num,LEAD(num,1) OVER(ORDER BY id) AS lead1, 
+# LEAD(num,2) OVER(ORDER BY id) AS lead2, 
+# LAG(num,1) OVER (ORDER BY id) AS lag1,
+# LAG(num,2) OVER (ORDER BY id) AS lag2 from logs
