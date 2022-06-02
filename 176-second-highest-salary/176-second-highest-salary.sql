93613250dc95446e995ad108382788bebd8243c2
Amit Sharma <lucifier.amit@gmail.com>
2022-06-02 09:17:52 +0530

    Time: 200 ms (83.02%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,10 +1,10 @@
 # Write your MySQL query statement below
 
-# select IFNULL(
-# (select (salary) as SecondHighestSalary from employee where salary Not In ( Select max(salary) from employee) order by salary desc limit 1),NULL) as SecondHighestSalary
-
 select IFNULL(
+(select (salary) as SecondHighestSalary from employee where salary Not In ( Select max(salary) from employee) order by salary desc limit 1),NULL) as SecondHighestSalary
 
-(select SecondHighestSalary from
-(select distinct(salary) as SecondHighestSalary , dense_rank() Over ( ORDER BY salary desc) as denserank  from employee)temp
-where temp.denserank = 2 ),NULL) as SecondHighestSalary
+# select IFNULL(
+
+# (select SecondHighestSalary from
+# (select distinct(salary) as SecondHighestSalary , dense_rank() Over ( ORDER BY salary desc) as denserank  from employee)temp
+# where temp.denserank = 2 ),NULL) as SecondHighestSalary
