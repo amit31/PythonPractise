3dfd873566322fb20935c77b4b2efee490ca036a
Amit Sharma <lucifier.amit@gmail.com>
2022-06-08 11:55:15 +0530

    Time: 853 ms (19.69%), Space: 0B (100.00%) - LeetHub

--- 184-department-highest-salary/184-department-highest-salary.sql
+++ 184-department-highest-salary/184-department-highest-salary.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 
 select temp.name as Department,temp.employeename as Employee ,temp.salary as Salary   from
-(select d.name,e.name as employeename,salary,dense_rank()   over ( partition by d.name order by salary desc ) as dr from employee e join department d on e.departmentId=d.id) temp where temp.dr=1
+(select d.name,e.name as employeename,salary,rank()   over ( partition by d.name order by salary desc ) as dr from employee e join department d on e.departmentId=d.id) temp where temp.dr=1
