0c582555d54c32c0aee7fce4f61d0b6715dac51b
Amit Sharma <lucifier.amit@gmail.com>
2022-06-22 13:20:14 +0530

    Time: 1145 ms (60.65%), Space: 0B (100.00%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -1,10 +1,25 @@
 # Write your MySQL query statement below
 
 
-# select p.product_id,product_name from product p
-# join sales s on p.product_id=s.product_id
-# and s.sale_date between "2019-01-01" and "2019-03-31"
 
+# select product_id,product_name from product 
+# where product_id not in 
+# (select product_id from sales where sale_date >"2019-03-31" or sale_date <"2018-12-31" )
+
+
+# with temp as 
+# (select p.product_id from product p left join sales s 
+# on p.product_id=s.product_id
+#  where sale_date >"2019-03-31" or sale_date <"2018-12-31" ) 
+# select product_id,product_name from temp where 
+
+# select product_id,product_name from product 
+# where product_id not in 
+
+with temp as 
+(select p.product_id,p.product_name,s.sale_date  from product p left join sales s 
+on p.product_id=s.product_id) 
 select product_id,product_name from product 
-where product_id not in 
-(select product_id from sales where sale_date >"2019-03-31" or sale_date <"2018-12-31" )
+where product_id not in (
+select temp.product_id from temp 
+where temp.sale_date >"2019-03-31" or temp.sale_date <"2018-12-31" or temp.sale_date is null)
