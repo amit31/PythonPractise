937cd359e3f8a850935a38160a572a4426d86969
Amit Sharma <lucifier.amit@gmail.com>
2022-06-25 11:18:10 +0530

    Time: 1152 ms (64.59%), Space: 0B (100.00%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -8,4 +8,4 @@ select user_id as buyer_id,join_date,count(o.order_date) as orders_in_2019 from 
 left join orders o on u.user_id=o.buyer_id
 and o.order_date 
 between "2019-01-01" and "2019-12-31" 
-group by user_id,buyer_id,join_date
+group by user_id,join_date
