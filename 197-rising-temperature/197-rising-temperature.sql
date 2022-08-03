312fc41f8cb9dc2e7e8735e845df02ca4eb0e227
Amit Sharma <lucifier.amit@gmail.com>
2022-08-03 12:48:22 +0530

    Time: 402 ms (89.54%), Space: 0B (100.00%) - LeetHub

--- 197-rising-temperature/197-rising-temperature.sql
+++ 197-rising-temperature/197-rising-temperature.sql
@@ -1,9 +1,9 @@
 # Write your MySQL query statement below
 
-# select id 
-# from
+#select id 
+#from
 # (select id,recordDate,temperature, lag(temperature)  over (order by recordDate) as prev_date_temp
-# from weather) temp where temp.temperature > temp.prev_date_temp
+# from weather) #temp where temp.temperature > temp.prev_date_temp
 
 # select id from (
 # select id,recordDate,temperature, lag(temperature)  over (order by recordDate ) as next_date_temp ,
@@ -11,7 +11,10 @@
 # from weather )temp where temp.temperature > temp.prev_date
 # and datediff(temp.recordDate,temp.prev_date)=1
 
+# select id from
+# (select id,recordDate,temperature, lag(temperature)  over (order by recordDate ) as next_date_temp ,
+# lag(recordDate) over (order by recordDate) as prev_date 
+# from weather)temp where datediff(temp.recordDate,temp.prev_date)=1 and temp.temperature>temp.next_date_temp
+
 select id from
-(select id,recordDate,temperature, lag(temperature)  over (order by recordDate ) as next_date_temp ,
-lag(recordDate) over (order by recordDate) as prev_date 
-from weather)temp where datediff(temp.recordDate,temp.prev_date)=1 and temp.temperature>temp.next_date_temp
+(select id,recordDate,temperature,lag(temperature) over (order by recordDate) as prev_date_temp,lag(recordDate) over (order by recordDate) as prev_date from weather ) temp where temp.temperature> temp.prev_date_temp and datediff(temp.recordDate,temp.prev_date)=1
