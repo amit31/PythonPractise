de85b96b98eee726e74576c3c5272003cb2a9fdb
Amit Sharma <lucifier.amit@gmail.com>
2022-08-03 11:33:14 +0530

    Time: 563 ms (82.99%), Space: 0B (100.00%) - LeetHub

--- 184-department-highest-salary/184-department-highest-salary.sql
+++ 184-department-highest-salary/184-department-highest-salary.sql
@@ -8,4 +8,4 @@
 
 select temp.name as Department,temp.employeename as Employee,salary
 from
-(select e.name as employeename,d.name ,salary , dense_rank () over ( partition by departmentID order by salary desc) as dr from employee e join department d on e.departmentId=d.id) temp where temp.dr=1
+(select e.name as employeename,d.name ,salary , rank () over ( partition by departmentID order by salary desc) as dr from employee e join department d on e.departmentId=d.id) temp where temp.dr=1
