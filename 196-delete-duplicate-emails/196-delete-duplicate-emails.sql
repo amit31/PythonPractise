ee342e0ddc3cc805e1b05c2e6b5ec5452ee4982e
Amit Sharma <lucifier.amit@gmail.com>
2022-08-03 12:33:36 +0530

    Time: 732 ms (84.79%), Space: 0B (100.00%) - LeetHub

--- 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
  delete p1
-FROM Person p1,
-    Person p2
+FROM Person p1
+    join Person p2 on p1.email=p2.email
 WHERE
     p1.Email = p2.Email
 and p1.id>p2.id
