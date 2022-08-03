9aa17f5e0f51333da2342f2d69ca8984905d8264
Amit Sharma <lucifier.amit@gmail.com>
2022-08-03 12:57:25 +0530

    Time: 579 ms (62.50%), Space: 0B (100.00%) - LeetHub

--- 511-game-play-analysis-i/511-game-play-analysis-i.sql
+++ 511-game-play-analysis-i/511-game-play-analysis-i.sql
@@ -1,4 +1,7 @@
 # Write your MySQL query statement below
 
 
-select player_id,min(event_date) as first_login from activity group by player_id
+#select player_id,min(event_date) as first_login from activity group by player_id
+select temp.player_id , temp.event_date as first_login from
+(select player_id, event_date,dense_rank() Over (partition by player_id order by event_date)  as dr from activity ) temp
+where temp.dr=1
