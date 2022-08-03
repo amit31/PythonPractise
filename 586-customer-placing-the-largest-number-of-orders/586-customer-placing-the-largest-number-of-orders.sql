98ed8573f275266994188d501ecf3c7da9886981
Amit Sharma <lucifier.amit@gmail.com>
2022-08-03 13:22:29 +0530

    Time: 541 ms (53.30%), Space: 0B (100.00%) - LeetHub

--- 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
@@ -5,8 +5,7 @@
 # group by customer_number order by 2 desc )t
 
 
-select t.customer_number from
-(select count(order_number) , customer_number  from orders 
- group by customer_number order by 1 desc limit 1)t
+select customer_number from 
 
+(select count(order_number) as cnt ,customer_number from orders group by customer_number order by cnt desc limit 1) t
 
