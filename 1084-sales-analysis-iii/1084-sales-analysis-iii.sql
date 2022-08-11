75db1a745ac630900bb3c942ae011a10d168ba20
Amit Sharma <lucifier.amit@gmail.com>
2022-08-11 17:22:14 +0530

    Time: 2870 ms (5.00%), Space: 0B (100.00%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -15,11 +15,14 @@
 
 # select product_id,product_name from product 
 # where product_id not in 
+ with cte as (
+ select p.product_id,s.sale_date from product p join sales s 
+on p.product_id=s.product_id where sale_date ),
+ temp as (
+select p.product_id,c.sale_date from product p  left join cte c on c.product_id=p.product_id where c.sale_date > "2019-03-31" or sale_date<"2018-12-31" or c.sale_date is null )
+select p.product_id,p.product_name from product p where p.product_id not in (select product_id from temp)
+ 
+ 
 
-with temp as 
-(select p.product_id,p.product_name,s.sale_date  from product p left join sales s 
-on p.product_id=s.product_id) 
-select product_id,product_name from product 
-where product_id not in (
-select temp.product_id from temp 
-where temp.sale_date >"2019-03-31" or temp.sale_date <"2018-12-31" or temp.sale_date is null)
+# select p.product_id,s.sale_date from product p join sales s 
+# on p.product_id=s.product_id where sale_date < "2019-01-01" or sale_date > "2019-03-31"
