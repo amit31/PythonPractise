376d492cfbf5da5856e60b9c39c5e10fdeeaf61b
Amit Sharma <lucifier.amit@gmail.com>
2022-08-11 18:18:43 +0530

    Time: 1686 ms (38.60%), Space: 0B (100.00%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -4,8 +4,9 @@
 #where filters the rows when used with order_date
 #and will give result with left join even when condition fails
 
-select user_id as buyer_id,join_date,count(o.order_date) as orders_in_2019 from  users u 
-left join orders o on u.user_id=o.buyer_id
-and o.order_date 
-between "2019-01-01" and "2019-12-31" 
-group by user_id,join_date
+with temp as (
+select user_id as buyer_id,join_date,count(order_date) as orders_in_2019
+from users u join orders o 
+on u.user_id=o.buyer_id where o.order_date > '2018-12-31' group by user_id,join_date )
+
+select u.user_id as buyer_id,u.join_date,ifnull(t.orders_in_2019,0) as orders_in_2019  from users u left join temp t on u.user_id=t.buyer_id
