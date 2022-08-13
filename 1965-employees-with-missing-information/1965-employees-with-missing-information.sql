49d8657cce7b23330d2b9554e3c101f78573ebb1
Amit Sharma <lucifier.amit@gmail.com>
2022-08-13 12:05:57 +0530

    Time: 648 ms (48.14%), Space: 0B (100.00%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,17 +1,6 @@
 # Write your MySQL query statement below
-
-
-select e.employee_id from employees e 
-left join salaries s on e.employee_id=s.employee_id
-
-where s.salary is NULL
-
-Union all
-
-select s.employee_id from salaries s
-left join employees e on s.employee_id=e.employee_id
-where e.name is null
-
-order by employee_id 
-
-
+select * from 
+( select e.employee_id from employees e left join salaries s on e.employee_id=s.employee_id where s.salary is NULL
+union 
+select s.employee_id from salaries s left join employees e on e.employee_id=s.employee_id where e.name is NULL ) temp 
+ order by temp.employee_id 
